b76d9626683991016b525d522e64631006468e79
ivanperez-7 <ivan.perez27@outlook.com>
2023-06-23 21:34:31 -0600

    modif .gitignore

---
+++ PrintPOS.py
@@ -0,0 +1,15 @@
+"""
+ARCHIVO PRINCIPAL.
+Inicia una QApplication y abre la ventana de iniciar sesión.
+"""
+from PyQt5.QtWidgets import QApplication, QStyleFactory
+from Login.App_Login import App_Login
+
+def main():
+    app = QApplication([])
+    app.setStyle(QStyleFactory.create('fusion'))
+    login = App_Login()
+    app.exec()
+
+if __name__ == '__main__':
+    main()
