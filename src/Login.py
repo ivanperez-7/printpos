6ad26afbcd740e75b9594da0b6badc7d1d6b9007
ivanperez-7 <ivan.perez27@outlook.com>
2023-09-01 19:09:37 -0600

    orden tabs + botón login

--- src/Login.py
+++ src/Login.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtGui, QtCore
 from PySide6.QtCore import Qt, Signal
 
 from utils.mydecorators import run_in_thread
@@ -106,3 +106,47 @@ class App_Login(QtWidgets.QWidget):
         
         self.close()
         self.mainWindow = VentanaPrincipal(conn)
+
+########################
+# WIDGET PERSONALIZADO #
+########################
+class LoginButton(QtWidgets.QPushButton):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.color1 = QtGui.QColor(240, 53, 218)
+        self.color2 = QtGui.QColor(61, 217, 245)
+
+        self._animation = QtCore.QVariantAnimation(
+            self,
+            valueChanged=self._animate,
+            startValue=0.00001,
+            endValue=0.9999,
+            duration=250
+        )
+
+    def _animate(self, value):
+        qss = """
+            font: 75 10pt "Microsoft YaHei UI";
+            font-weight: bold;
+            color: rgb(255, 255, 255);
+            border-style: solid;
+            border-radius: 15px;
+            outline: 0px;
+        """
+        grad = """background-color: qlineargradient(
+                                        spread:pad, x1:0, y1:0, x2:1, y2:0,
+                                        stop:0 {color1}, stop:{value} {color2}, stop: 1.0 {color1});""".format(
+            color1=self.color1.name(), color2=self.color2.name(), value=value)
+        qss += grad
+        self.setStyleSheet(qss)
+
+    def enterEvent(self, event):
+        self._animation.setDirection(QtCore.QAbstractAnimation.Forward)
+        self._animation.start()
+        super().enterEvent(event)
+
+    def leaveEvent(self, event):
+        self._animation.setDirection(QtCore.QAbstractAnimation.Backward)
+        self._animation.start()
+        super().leaveEvent(event)
