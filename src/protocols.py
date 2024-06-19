8c50920465df3d84799bf43157301317df54e433
ivanperez-7 <ivan.perez27@outlook.com>
2024-06-19 03:47:22 -0600

    clases simples para protocols.py

--- src/protocols.py
+++ src/protocols.py
@@ -1,8 +1,8 @@
 """ Módulo para proveedores de type hinting. """
 from __future__ import annotations
-from typing import Protocol, TYPE_CHECKING
+from typing import TYPE_CHECKING
 
-from PySide6.QtWidgets import QWidget, QMainWindow
+from PySide6.QtWidgets import QWidget
 from PySide6.QtCore import Signal
 
 if TYPE_CHECKING:
@@ -10,24 +10,16 @@ if TYPE_CHECKING:
     from utils.mydataclasses import Usuario
 
 
-class _ModuloProtocol(Protocol):
+class HasConnUser:
+    """ Clase para type-hinting de atributos persistentes `conn` y `user`. """
     conn: sql.Connection
     user: Usuario
+    
 
-_ProtocolMeta = type(_ModuloProtocol)
-_WidgetMeta = type(QWidget)
-
-class _CustomWidgetMeta(_WidgetMeta, _ProtocolMeta):
-    pass
-
-class ModuloPrincipal(QWidget, _ModuloProtocol, metaclass=_CustomWidgetMeta):
+class ModuloPrincipal(QWidget, HasConnUser):
     """ Subclase de QWidget para módulo principal, con atributos persistentes
         `conn`, `user` y señal `go_back`. """
     go_back: Signal = Signal()
-
-class HasConnUser(QMainWindow, _ModuloProtocol, metaclass=_CustomWidgetMeta):
-    """ Subclase de QMainWindow con atributos persistentes `conn` y `user`. """
-    ...
 
 
 __all__ = ['ModuloPrincipal', 'HasConnUser']
