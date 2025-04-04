ca9abbd9f477d76057c3071f63cc8ac5d3f5765f
Iván Pérez <ivan.perez27@outlook.com>
2025-04-03 20:37:21 -0600

    Nueva rama para ORM son SQLAlchemy

--- src/sql/core.py
+++ src/sql/core.py
@@ -2,6 +2,8 @@
 
 import fdb
 from haps import egg
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from config import INI
 from interfaces import IDatabaseConnection
@@ -35,3 +37,13 @@ def conectar_firebird(usuario: str, psswd: str, rol: str = None) -> FirebirdConn
         txt, sqlcode, gdscode = err.args
         print('\nconectar_db() {\n', sqlcode, gdscode, '\n' + txt + '\n}')
         raise err
+
+
+def conectar_firebird_sqlalchemy(usuario: str, psswd: str, rol: str = None):
+    """ Crea conexión a base de datos y regresa conexión a DB.
+    Levanta sql.FirebirdError, por lo que siempre se debe usar en un bloque `try-except`.
+    """
+    connection_string = f'firebird+fdb://{usuario}:{psswd}@{INI.NOMBRE_SERVIDOR}:3050/PrintPOS.fdb?charset=UTF8&role={rol}'
+    engine = create_engine(connection_string)
+    Session = sessionmaker(bind=engine)
+    return Session()
